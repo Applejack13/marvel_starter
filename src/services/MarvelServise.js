60974b16bc232954265b4e2855c0b073d7a65352
Vadym <andreeshinvadim13@gmail.com>
2023-08-12 14:52:53 +0300

    Lesson 150
    Add a spinner and an error messages

--- src/services/MarvelServise.js
+++ src/services/MarvelServise.js
@@ -13,14 +13,33 @@ class MarvelService {
     return await res.json()
   }
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const response = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     )
+    return response.data.results.map(this._transformCharacter)
   }
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+  getCharacter = async (id) => {
+    const response = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    )
+    return this._transformCharacter(response.data.results[0])
+  }
+
+  _transformCharacter = (char) => {
+    if (!char.description) {
+      char.description = "No description"
+    } else {
+      char.description = char.description.substring(0, 100)
+    }
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    }
   }
 }
 
